4ae1ce0cabf9204c61bd826b527a8644ed7087cd
cywang <cywang@mail.nankai.edu.cn>
2022-08-18 12:22:59 +0000

    update scripts

---
+++ bash_scripts/train_vas_transformer.sh
@@ -0,0 +1,2 @@
+
+/home/ubuntu/miniconda3/envs/specvqgan/bin/python train.py --base configs/vas_transformer.yaml -t True --gpus 0,1,2,3 model.params.first_stage_config.params.ckpt_path=/modelblob/users/v-chengw/models/specvqgan/2022-08-15T15-03-10_vas_codebook/checkpoints/last.ckpt 
